dff7e3a24b9982f1f5fc956070c012647ed14f6d
tycoon <huynhanhtri29041701@gmail.com>
2025-12-02 19:06:38 +0700

    Delete src directory

--- src/app/playlist/[id]/page.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PlaylistDetailClient from "./PlaylistDetailClient";
-
-export default function PlaylistDetailPage({ params }) {
-  const { id } = React.use(params);
-
-  return <PlaylistDetailClient playlistId={id} />;
-}
--- src/hooks/useGetSongById.js
+++
@@ -1,6 +0,0 @@
-"use client";
-
-n// Stubbed out: placeholder hook removed during cleanup.
-export default function useGetSongById(id) {
-  return { song: null, isLoading: false };
-}
