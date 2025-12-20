7ff12f8d53ffac1772f88a6725fa8b8f8c499e1b
unknown <long.hn.64cntt@ntu.edu.vn>
2025-12-20 11:07:18 +0700

    Sửa trạng thái online/offline

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,24 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-const customStorage = {
-  getItem: (key) => {
-    if (typeof window !== 'undefined') {
-      return window.sessionStorage.getItem(key)
-    }
-    return null
-  },
-  setItem: (key, value) => {
-    if (typeof window !== 'undefined') {
-      window.sessionStorage.setItem(key, value)
-    }
-  },
-  removeItem: (key) => {
-    if (typeof window !== 'undefined') {
-      window.sessionStorage.removeItem(key)
-    }
-  },
-}
+
 // Kiểm tra lỗi biến môi trường ngay lập tức
 if (!supabaseUrl || !supabaseKey) {
   console.error("SUPABASE_URL hoặc ANON_KEY bị thiếu! Kiểm tra Environment Variables trên Vercel.");
@@ -27,10 +10,13 @@ if (!supabaseUrl || !supabaseKey) {
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
-    // Dùng sessionStorage để tự động logout khi đóng tab/trình duyệt
-    storage: typeof window !== 'undefined' ? window.sessionStorage : null, 
+    storage: typeof window !== 'undefined' ? window.localStorage : null, // Đổi từ sessionStorage sang localStorage
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true,
   },
 })
+
+if (typeof window !== 'undefined') {
+    window.supabase = supabase;
+}
